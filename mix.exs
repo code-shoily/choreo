9a3a3a72c38d01c5f95138544c5e92289b6627a2
Mafinar Khan <mafinar@gmail.com>
2026-04-26 16:09:36 -0400

    Add nimbleoptions

--- mix.exs
+++ mix.exs
@@ -52,6 +52,7 @@ defmodule Choreo.MixProject do
   defp deps do
     [
       {:yog_ex, "~> 0.97.1"},
+      {:nimble_options, "~> 1.1"},
       {:ex_doc, "~> 0.40", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
